fc7b6579f5354f1337a93e9c9580c15daffd7a4f
Christoph Danzer <c0mp4ct.23@gmail.com>
2022-01-18 08:45:09 +0100

    ubuntu-cd-home-pc

---
+++ auto-git.sh
@@ -0,0 +1,4 @@
+cd /home/compact/.steam/root/steamapps/compatdata/252950/pfx/drive_c/users/steamuser/AppData/Roaming/bakkesmod/bakkesmod/ 
+git add -A
+git commit -am 'ubuntu-cd-home-pc'
+git push --set-upstream origin 
