7c6c65a7511f0409e33e6278c7a8613e7d0bc57b
Christoph Danzer <c0mp4ct.23@gmail.com>
2022-01-18 08:51:57 +0100

    ubuntu-cd-home-pc

--- auto-git.sh
+++ auto-git.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd /home/compact/.steam/root/steamapps/compatdata/252950/pfx/drive_c/users/steamuser/AppData/Roaming/bakkesmod/bakkesmod/ 
 git add -A
 git commit -am 'ubuntu-cd-home-pc'
